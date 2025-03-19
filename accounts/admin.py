a5758f86ab296704bd54f30f3d46de39b9a73a1b
giorgiabes <abesadze.george@gmail.com>
2025-03-19 18:34:49 +0400

    user authentication

--- accounts/admin.py
+++ accounts/admin.py
@@ -19,4 +19,5 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
     add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
 
+
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -5,11 +5,19 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        modle = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
